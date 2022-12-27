65f7ed6b5f93147265c7343ba94778aef3c2e0ed
Khondaker Tasnia Hoque <60292796+Tasnia16@users.noreply.github.com>
2022-12-27 12:42:18 +0600

    Update plaintextToHtmlConverter.java

--- workshop/plaintextToHtmlConverter.java
+++ workshop/plaintextToHtmlConverter.java
@@ -24,7 +24,6 @@ public class plaintextToHtmlConverter {
     private String basicHtmlEncode(String source) {
         result = new ArrayList<>();
         convertedLine = new ArrayList<>();
-        //characterToConvert = stashNextCharacterAndAdvanceThePointer();
         convertion(source);
 
         addANewLine();
